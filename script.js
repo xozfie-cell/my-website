0df480dd508ce5fa9b3459017fa3542d69cefe07
Firie <xozxoai@gmail.com>
2025-09-11 13:49:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function smile() {
 function flirting() {
   alert("Coba salto!");
 }
+
+body { height: 100%;  orgin: 0; }
+body {
+  font-family: Arial, sans-serif;
+  background: linear-gradient(135deg, #ff7e5f, #feb47b);
+  color: #111;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background 0.6s ease;
+}
